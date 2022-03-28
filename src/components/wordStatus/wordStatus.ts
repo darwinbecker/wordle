0ad35a2375de6c7df0c07201d5f6b8f62dddbe6f
Darwin Becker <darwinbecker@web.de>
2022-03-28 17:59:44 +0200

    fixed bug: improved wordstatus feedback
    now also checks words that have the same letter multiple times

--- src/components/wordStatus/wordStatus.ts
+++ src/components/wordStatus/wordStatus.ts
@@ -18,8 +18,36 @@ export const checkstatus = (guess: string, solution: string): StatusType[] => {
 
     guessChars.map((char, index) => {
         if (status[index] == 'semi') {
+            const charCount = solution.split(char);
+            console.log("----------start----------")
+
+            let guessedCorrect = false;
+            let guessedCorrect2 = false;
+
+            const i = solutionChars.indexOf(char);
+
+            // if (charCount.length - 1 == 1)
             if (status[solutionChars.indexOf(char)] == 'correct') {
                 status[index] = 'wrong';
+                guessedCorrect = true;
+            }
+
+            if (charCount.length - 1 == 2) {
+                if (status[solutionChars.indexOf(char, i + 1)] == 'correct' && guessedCorrect) {
+                    status[index] = 'wrong';
+                    guessedCorrect2 = true;
+                } else {
+                    status[index] = 'semi';
+                }
+            }
+
+            if (charCount.length - 1 == 3) {
+                const i2 = solutionChars.indexOf(char, i + 1);
+                if (status[solutionChars.indexOf(char, i2 + 1)] == 'correct' && guessedCorrect2) {
+                    status[index] = 'wrong';
+                } else {
+                    status[index] = 'semi';
+                }
             }
         }
     });
