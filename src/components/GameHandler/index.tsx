f1c658fca7c0b8af42e8bbdfe99eedd9aa8f449d
Darwin Becker <darwinbecker@web.de>
2022-05-10 00:46:13 +0200

    cleanup

--- src/components/GameHandler/index.tsx
+++ src/components/GameHandler/index.tsx
@@ -1,5 +1,4 @@
 export { GameHandler } from './GameHandler';
 export { GameModeHandlerService } from './GameModeHandlerService';
 export type { GameModeType } from './GameHandler';
-export { InputHandler } from './InputHandler';
 export { WinService } from './WinService';
