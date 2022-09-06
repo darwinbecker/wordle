620d36b4712816329dfbb0ca150eb52d98af1504
Darwin Becker <darwinbecker@web.de>
2022-09-06 23:28:30 +0200

    cleanup

--- src/components/Animations/Confetti.ts
+++ src/components/Animations/Confetti.ts
@@ -1,20 +1,22 @@
 // https://www.npmjs.com/package/canvas-confetti
-import confetti from 'canvas-confetti';
+import confetti from "canvas-confetti";
 
 export const Confetti = () => {
-    const canvas = document.getElementById('confetti-canvas') as HTMLCanvasElement;
-    var myConfetti = confetti.create(canvas, {
-        resize: true,
-        useWorker: true
-    });
-    myConfetti({
-        particleCount: 400,
-        spread: 60,
-        // startVelocity: 60,
-        gravity: 0.9,
-        origin: {
-            x: 0.5,
-            y: 0.8
-        }
-    });
-}
+  const canvas = document.getElementById(
+    "confetti-canvas"
+  ) as HTMLCanvasElement;
+  var myConfetti = confetti.create(canvas, {
+    resize: true,
+    useWorker: true,
+  });
+  myConfetti({
+    particleCount: 400,
+    spread: 60,
+    // startVelocity: 60,
+    gravity: 0.9,
+    origin: {
+      x: 0.5,
+      y: 0.8,
+    },
+  });
+};
