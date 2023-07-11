8a43d9bdff1e505af68f9d41ed8a44c7390a24f6
Darwin Becker <darwinbecker@web.de>
2023-07-11 21:14:48 +0200

    added more tests

--- src/libs/WordStatus/WordStatus.test.ts
+++ src/libs/WordStatus/WordStatus.test.ts
@@ -36,4 +36,50 @@ describe("check word status of solution word: 'ADMIN'", () => {
         expect(status).toContain("wrong" && "correct" && "semi");
         expect(status).toEqual(["wrong", "correct", "correct", "wrong", "semi"]);
     });
+
+    test('check status of random word NNMEE', () => {
+        const status = checkstatus("NNMEE", "ADMIN");
+        expect(status.length).toEqual(5);
+        expect(status).toContain("wrong" && "correct" && "semi");
+        expect(status).toEqual(["semi", "wrong", "correct", "wrong", "wrong"]);
+    });
+})
+
+
+describe("check word status of solution word: 'ALLEE'", () => {
+    test('check status of REGAL', () => {
+        const status = checkstatus("REGAL", "ALLEE");
+        expect(status.length).toEqual(5);
+        expect(status).toContain("wrong" && "semi");
+        expect(status).toEqual(["wrong", "semi", "wrong", "semi", "semi"]);
+    });
+
+    test('check status of ETAGE', () => {
+        const status = checkstatus("ETAGE", "ALLEE");
+        expect(status.length).toEqual(5);
+        expect(status).toContain("semi" && "wrong" && "correct");
+        expect(status).toEqual(["semi", "wrong", "semi", "wrong", "correct"]);
+    });
+
+    test('check status of ALTAR', () => {
+        const status = checkstatus("ALTAR", "ALLEE");
+        expect(status.length).toEqual(5);
+        expect(status).toContain("correct" && "wrong");
+        expect(status).toEqual(["correct", "correct", "wrong", "wrong", "wrong"]);
+    });
+
+    test('check status of ENTEN', () => {
+        const status = checkstatus("ENTEN", "ALLEE");
+        expect(status.length).toEqual(5);
+        expect(status).toContain("correct" && "semi" && "wrong");
+        expect(status).toEqual(["semi", "wrong", "wrong", "correct", "wrong"]);
+    });
+
+    
+    test('check status of random word ELMOO', () => {
+        const status = checkstatus("ELMLL", "ALLEE");
+        expect(status.length).toEqual(5);
+        expect(status).toContain("wrong" && "correct" && "semi");
+        expect(status).toEqual(["semi", "correct", "wrong", "semi", "semi"]);
+    });
 })
